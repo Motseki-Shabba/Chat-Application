2306c65427c33a6f83e62af768476ef25fbbddd8
Motsek-shabba <motsekitshabalala501@gmail.com>
2025-02-14 15:50:28 +0200

    js file

---
+++ javascript/sign-up.js
@@ -0,0 +1,31 @@
+function signUp() {
+  let firstname, lastname, email, password;
+
+  firstname = document.getElementById("firstname").value;
+  lastname = document.getElementById("lastname").value;
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+
+  let signUpUsers_Records = new Array();
+
+  signUpUsers_Records = JSON.parse(localStorage.getItem("users"))
+    ? JSON.parse(localStorage.getItem("users"))
+    : [];
+
+  if (
+    signUpUsers_Records.some((n) => {
+      return n.email === email;
+    })
+  ) {
+    alert("User exists");
+  } else {
+    signUpUsers_Records.push({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      password: password,
+    });
+
+    localStorage.setItem("users", JSON.stringify(signUpUsers_Records));
+  }
+}
