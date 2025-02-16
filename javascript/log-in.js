aba7531936140b8bc0a0e3ddfcf351a03a85b56a
Motsek-shabba <motsekitshabalala501@gmail.com>
2025-02-16 14:11:09 +0200

    log-in-functionality

---
+++ javascript/log-in.js
@@ -0,0 +1,27 @@
+function logIn() {
+  let firstname, password;
+
+  //Getting user input values
+  firstname = document.getElementById("firstname").value;
+  password = document.getElementById("password").value;
+
+  //Restoring stored users from the LocalStorage or return empty array
+  let logInRecords = JSON.parse(localStorage.getItem("users")) || [];
+
+  //Finding the user with matching credentials
+  const loggedInUser = logInRecords.find(
+    (user) => user.firstname === firstname && user.password === password
+  );
+
+  if (loggedInUser) {
+    //login successful and redirect to chat panel
+    alert("Login Successful");
+
+    localStorage.setItem("email", loggedInUser.email);
+    localStorage.setItem("lastname", loggedInUser.lastname);
+    window.location.href = "index.html";
+  } else {
+    //else, notify the user to check the credentials correctly
+    alert("Login failed. Please check your login credentials");
+  }
+}
